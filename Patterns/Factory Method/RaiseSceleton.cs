9b755990d95126c8bd57663f63c1f2734ed1d813
HappyFucker <17487175+Molphar@users.noreply.github.com>
2024-05-28 01:31:12 +0300

    Add Factory method

---
+++ Patterns/Factory Method/RaiseSceleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patterns.Factory_Method
+{
+    class RaiseSceleton : RaiseUndead
+    {
+        public override Undead Raise()
+        {
+            return new Sceleton();
+        }
+    }
+}
