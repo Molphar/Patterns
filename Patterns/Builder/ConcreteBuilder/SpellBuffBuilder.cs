b72895deed0e0d5b6e9e36c25dcc3c859a5433ce
HappyFucker <17487175+Molphar@users.noreply.github.com>
2024-05-28 01:31:13 +0300

    Add factory mathod and builder

---
+++ Patterns/Builder/ConcreteBuilder/SpellBuffBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Patterns.Builder.ConcreteBuilder
+{
+    public class SpellBuffBuilder : SpellBuilder
+    {
+        private Spell _spell = new Spell();
+
+        public SpellBuffBuilder(string player, string target, string worldParams) 
+            : base(player, target, worldParams)
+        {
+            _spell = new Spell();
+        }
+
+        public override void BuildRoles()
+        {
+            _spell.Player = player;
+            _spell.Target = target;
+            if (!string.IsNullOrEmpty(worldParams))
+            {
+                _spell.EffectArea = "good!";
+            }
+        }
+
+        public override void BuildAttribute()
+        {
+            _spell.Attribut = "lolol";
+        }
+
+        public override void BuildEffects()
+        {
+            _spell.AdditionalEffects = "DOT";
+        }
+
+        public override void BuildTime()
+        {
+            _spell.Duration = 60;
+            _spell.Casting = 60;
+        }
+
+        public override Spell GetSpell()
+        {
+            return _spell;
+        }
+
+        public override void Reset()
+        {
+            _spell = new Spell();
+        }
+    }
+}
---
+++ Patterns/Builder/Spell.cs
@@ -0,0 +1,23 @@
+﻿namespace Patterns.Builder
+{
+    public class Spell
+    {
+        public string Player { get; set; }
+
+        public string Target { get; set; }
+
+        public string Attribut { get; set; }
+
+        public string Element { get; set; }
+
+        public string EffectArea { get; set; }
+
+        public float Intensity { get; set; }
+
+        public string AdditionalEffects { get; set; }
+
+        public float Duration { get; set; }
+
+        public float Casting { get; set; }
+    }
+}
